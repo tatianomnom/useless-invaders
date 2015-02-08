4bf4453bb920a4b75892d94a8140478663389af1
tatianomnom <orangium@gmail.com>
2015-02-08 19:50:34 +0300

    Moar equipment tests!

--- src/test/groovy/com/tatianomnom/uselessinvaders/EquipmentSpec.groovy
+++ src/test/groovy/com/tatianomnom/uselessinvaders/EquipmentSpec.groovy
@@ -7,7 +7,6 @@ class EquipmentSpec extends Specification {
 
     private Hero hero = new Hero(100, [agility: 100, strength: 20, intelligence: 0])
 
-
     def "Item in the inventory doesn't modify hero stats"() {
         given:
         Equipment equipment = new Equipment(equipmentStats)
@@ -17,8 +16,10 @@ class EquipmentSpec extends Specification {
         hero.getStats().equals(expectedStats)
 
         where:
-        equipmentStats             | expectedStats
-        [agility: 0, strength: 80] | [agility: 100, strength: 20, intelligence: 0]
+        equipmentStats               | expectedStats
+        [agility: 0, strength: 80]   | [agility: 100, strength: 20, intelligence: 0]
+        [agility: 0, strength: -5]   | [agility: 100, strength: 20, intelligence: 0]
+        [agility: -10, strength: -5] | [agility: 100, strength: 20, intelligence: 0]
 
     }
 
@@ -32,8 +33,10 @@ class EquipmentSpec extends Specification {
         hero.getStats().equals(expectedStats)
 
         where:
-        equipmentStats             | expectedStats
-        [agility: 0, strength: 80] | [agility: 100, strength: 100, intelligence: 0]
+        equipmentStats               | expectedStats
+        [agility: 0, strength: 80]   | [agility: 100, strength: 100, intelligence: 0]
+        [agility: 10, strength: -10] | [agility: 110, strength: 10, intelligence: 0]
+        [intelligence: 1000]         | [agility: 100, strength: 20, intelligence: 1000]
     }
 
     def "Unequipped item doesn't modify hero stats no more"() {
@@ -47,8 +50,9 @@ class EquipmentSpec extends Specification {
         hero.getStats().equals(expectedStats)
 
         where:
-        equipmentStats             | expectedStats
-        [agility: 0, strength: 80] | [agility: 100, strength: 20, intelligence: 0]
+        equipmentStats               | expectedStats
+        [agility: 0, strength: 80]   | [agility: 100, strength: 20, intelligence: 0]
+        [agility: -10, strength: -5] | [agility: 100, strength: 20, intelligence: 0]
     }
 
 
